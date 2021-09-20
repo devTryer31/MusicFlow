451d98329f1e44a7ba2b4e1a402235c5721887ca
dolbilov <dolbilov.kirill@gmail.com>
2021-09-20 17:44:15 +0300

    Initial commit

---
+++ MusicBot/Bot.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Extensions.Polling;
+
+namespace MusicBot
+{
+    public static class Bot
+    {
+        private static TelegramBotClient? _botClient;
+        private static User? _me;
+        private static CancellationTokenSource? _cts;
+
+        public static bool IsStarted { get; private set; }
+
+        public static async void StartBot()
+        {
+            if (IsStarted)
+                return;
+
+            _botClient = new TelegramBotClient(Configuration.Token);
+            _me = await _botClient.GetMeAsync();
+            _cts = new CancellationTokenSource();
+            
+            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+            _botClient.StartReceiving(
+                new DefaultUpdateHandler(Handlers.HandleUpdateAsync, Handlers.HandleErrorAsync),
+                _cts.Token
+            );
+
+            Console.WriteLine("Bot started successfully\nPress any key to stop...");
+            IsStarted = true;
+        }
+
+        public static void StopBot()
+        {
+            if (!IsStarted)
+                throw new TaskCanceledException("Can't stop bot when it isn't started");
+            
+            // Send cancellation request to stop bot
+            _cts?.Cancel();
+
+            Console.WriteLine("Bot stopped");
+            IsStarted = false;
+        }
+
+        public static async void SendMessageAsync(ITelegramBotClient botClient, Update update, long chatId, string msg)
+        {
+            await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: msg,
+                replyToMessageId: update.Message.MessageId
+            );
+        }
+    }
+}
+
---
+++ MusicBot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MusicBot
+{
+    internal static class Program
+    {
+        private static async Task Main()
+        {
+            // load data from file
+            Configuration.Initialize();
+            
+
+            #region Start Telegram Bot
+
+            try
+            {
+                Bot.StartBot();
+                await SpotifyManager.Launch();
+                Console.ReadKey();
+                Bot.StopBot();
+                SpotifyManager.Stop();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            #endregion
+        }
+    }
+}
