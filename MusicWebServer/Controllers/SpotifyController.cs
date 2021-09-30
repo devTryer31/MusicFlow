f15aba3d634fb176d5814566ad6df06025abaead
Валера Герасимов <valera.gs@bk.ru>
2021-10-01 01:15:30 +0300

    Initial server items added.

---
+++ MusicWebServer/Controllers/SpotifyController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MusicWebServer.Controllers
+{
+	public class SpotifyController : Controller
+	{
+		public IActionResult GetToken(string stringRequest)
+		{
+			return View();
+		}
+	}
+}
