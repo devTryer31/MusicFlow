37c3ad4b2911f866ab59c33a5103176e0377c0db
dolbilov <dolbilov.kirill@gmail.com>
2021-10-22 13:33:53 +0300

    Config file was changed to .json

---
+++ MusicBotV2/Configuration.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+using System;
+
+namespace MusicBotV2
+{
+    [Serializable]
+    public class Configuration
+    {
+        public string? Token { get; init; }
+        public string? ClientId { get; init; }
+        public string? ClientSecret { get; init; }
+        public string? IFAPIKey { get; init; }
+    }
+}
