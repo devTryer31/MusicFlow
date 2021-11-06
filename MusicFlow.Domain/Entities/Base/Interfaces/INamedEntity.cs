406bd5f678c0e218450c2a014aec2349f5b5e06e
Валера Герасимов <valera.gs@bk.ru>
2021-11-06 22:24:50 +0300

    Clearing domain models.

--- MusicFlow.Domain/Entities/Base/Interfaces/INamedEntity.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MusicFlow.Domain.Entities.Base.Interfaces
-{
-	public interface INamedEntity : IEntity
-	{
-		string Name { get; set; }
-	}
-}
--- MusicFlow.Domain/Entities/Chat.cs
+++ MusicFlow.Domain/Entities/Chat.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
 using System.Diagnostics.CodeAnalysis;
 using MusicFlow.Domain.Entities.Base;
 
@@ -8,16 +9,48 @@ namespace MusicFlow.Domain.Entities
 	public class Chat : Entity
 	{
 		public long TelegramChatId { get; set; }
-		
-		public string Token { get; set; }
 
-		public long? HostUserId { get; set; }
+		public string Token { get; private set; }
 
-		public string RefreshToken { get; set; }
+		public long? HostUserId { get; private set; }
 
-		public int? ExpiresIn { get; set; }
+		public string RefreshToken { get; private set; }
+
+		public int? ExpiresIn { get; private set; }
 
 		[Column(TypeName = "long")]
-		public DateTime? CreatedAt { get; set; }
+		public DateTime? CreatedAt { get; private set; }
+
+		[NotMapped] 
+		public bool IsHostEstablished { get; private set; }
+
+		public void RemoveHost()
+		{
+			HostUserId = null;
+			RefreshToken = null;
+			ExpiresIn = null;
+			Token = null;
+			IsHostEstablished = false;
+		}
+
+		public void SetHost(long? hostUserID, string refreshToken, int? expiresIn, string token, DateTime? createdAt)
+		{
+			HostUserId = hostUserID;
+			RefreshToken = refreshToken;
+			ExpiresIn = expiresIn;
+			Token = token;
+			CreatedAt = createdAt;
+			IsHostEstablished = true;
+		}
+
+		public void UpdateHost(string refreshToken, int? expiresIn, string token, DateTime? updatedAt)
+		{
+			if (!IsHostEstablished)
+				throw new InvalidOperationException("The host cannot be updated if it is not set.");
+			RefreshToken = refreshToken;
+			ExpiresIn = expiresIn;
+			Token = token;
+			CreatedAt = updatedAt;
+		}
 	}
 }
